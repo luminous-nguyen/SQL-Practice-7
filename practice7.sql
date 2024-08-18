25f0a648c64e020190b834ca26b8b4c87a8936b5
luminous-nguyen <127753982+luminous-nguyen@users.noreply.github.com>
2024-08-18 22:36:51 +0700

    Update practice7.sql

--- practice7.sql
+++ practice7.sql
@@ -27,6 +27,41 @@ FROM monthly_cards_issued
 ORDER BY FIRST_VALUE (issued_amount) 
          OVER(PARTITION BY card_name 
               ORDER BY issue_year,issue_month) DESC
+-- Ex 3
+SELECT user_id,
+       spend,
+       transaction_date
+FROM(
+      SELECT user_id,
+             spend,
+             transaction_date,
+             ROW_NUMBER() 
+             OVER(PARTITION BY user_id ORDER BY transaction_date) as rank_transaction
+      FROM transactions
+     ) CTE 
+WHERE rank_transaction = 3
+-- Ex 4
+SELECT transaction_date,
+       user_id,
+       COUNT(transaction_rank) AS purchase_count
+FROM (
+       SELECT product_id,	
+              user_id,	
+              spend,	
+              transaction_date,
+              DENSE_RANK() 
+              OVER(PARTITION BY user_id ORDER BY transaction_date DESC) as transaction_rank
+       FROM user_transactions
+      ) CTE 
+WHERE transaction_rank =1
+GROUP BY transaction_date, user_id
+-- Ex 5
+SELECT user_id,
+       tweet_date,
+       ROUND(AVG(tweet_count) 
+             OVER(PARTITION BY user_id ORDER BY tweet_date 
+                  ROWS BETWEEN 2 PRECEDING AND CURRENT ROW),2) as rolling_avg_3d
+FROM tweets
 
 
 
