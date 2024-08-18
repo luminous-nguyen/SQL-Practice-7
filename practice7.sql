f694d7f68acc112f5aec1f51ccebc080369e3b5b
luminous-nguyen <127753982+luminous-nguyen@users.noreply.github.com>
2024-08-18 17:12:21 +0700

    Update practice7.sql

--- practice7.sql
+++ practice7.sql
@@ -1,1 +1,34 @@
+-- Ex 1
+SELECT 
+       EXTRACT(YEAR FROM transaction_date) AS year,
+       PRODUCT_ID,
+       SPEND AS curr_year_spend,
+       LAG(SPEND) OVER (
+                        PARTITION BY PRODUCT_ID 
+                        ORDER BY PRODUCT_ID, EXTRACT(YEAR FROM transaction_date)
+                        ) AS prev_year_spend,
+       ROUND(((spend/LAG(SPEND) OVER (
+                        PARTITION BY PRODUCT_ID 
+                        ORDER BY EXTRACT(YEAR FROM transaction_date)
+                        )
+              ) -1)*100,2)
+                    
+FROM user_transactions
+
+-- Ex 2
+SELECT
+      DISTINCT
+       card_name,
+       FIRST_VALUE (issued_amount) 
+       OVER(PARTITION BY card_name 
+              ORDER BY issue_year,issue_month) AS issued_amount
+      
+FROM monthly_cards_issued
+ORDER BY FIRST_VALUE (issued_amount) 
+         OVER(PARTITION BY card_name 
+              ORDER BY issue_year,issue_month) DESC
+
+
+
+
 
